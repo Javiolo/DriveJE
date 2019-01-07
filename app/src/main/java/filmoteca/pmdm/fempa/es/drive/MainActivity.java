227c77bde85a20d2183a24f85b646ccc41b5d399
Javier Gomez <jagomezmarco16@gmail.com>
2019-01-07 19:07:13 +0100

    Driveeee 0.1

---
+++ app/src/main/java/filmoteca/pmdm/fempa/es/drive/MainActivity.java
@@ -0,0 +1,13 @@
+package filmoteca.pmdm.fempa.es.drive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
